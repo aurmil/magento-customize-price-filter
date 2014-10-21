0aa13a851fb5a8af5710376123f07a90bb054613
Aurélien Millet <aurmil@users.noreply.github.com>
2014-10-21 17:53:31 +0200

    Fix on filtering products with range-edge prices
    eg: when setting "no" to "substract 0.01...", "300.00 - 399.99" is
    rounded into "300.00 - 400.00"
    products with prices equal to "400" were not correctly filtered (they
    were not added to the collection)

--- app/code/community/Aurmil/CustomizePriceFilter/Model/Catalog/Resource/Layer/Filter/Price.php
+++ app/code/community/Aurmil/CustomizePriceFilter/Model/Catalog/Resource/Layer/Filter/Price.php
@@ -10,39 +10,100 @@ extends Mage_Catalog_Model_Resource_Layer_Filter_Price
 {
     public function applyFilterToCollection($filter, $from, $to)
     {
-        if (!method_exists($filter, 'usePriceRanges') || !$filter->usePriceRanges()) {
-            return parent::applyFilterToCollection($filter, $from, $to);
-        }
+        if (method_exists($filter, 'usePriceRanges') && $filter->usePriceRanges()) {
+            if (('' === $from) && ('' === $to)) {
+                return $this;
+            }
 
-        if (('' === $from) && ('' === $to)) {
-            return $this;
-        }
+            $collection = $filter->getLayer()->getProductCollection();
+            $collection->addPriceData($filter->getCustomerGroupId(), $filter->getWebsiteId());
 
-        $collection = $filter->getLayer()->getProductCollection();
-        $collection->addPriceData($filter->getCustomerGroupId(), $filter->getWebsiteId());
+            $select     = $collection->getSelect();
+            $response   = $this->_dispatchPreparePriceEvent($filter, $select);
 
-        $select     = $collection->getSelect();
-        $response   = $this->_dispatchPreparePriceEvent($filter, $select);
+            $table      = $this->_getIndexTableAlias();
+            $additional = join('', $response->getAdditionalCalculations());
+            $rate       = $filter->getCurrencyRate();
+            $priceExpr  = new Zend_Db_Expr("(({$table}.min_price {$additional}) * {$rate})");
 
-        $table      = $this->_getIndexTableAlias();
-        $additional = join('', $response->getAdditionalCalculations());
-        $rate       = $filter->getCurrencyRate();
-        $priceExpr  = new Zend_Db_Expr("(({$table}.min_price {$additional}) * {$rate})");
+            if ('' !== $to) {
+                $to = (int)$to;
+                if ($from == $to) {
+                    $to += 1;
+                }
+            }
 
-        if ('' !== $to) {
-            $to = (int)$to;
-            if ($from == $to) {
-                $to += 1;
+            if ('' !== $from) {
+                $select->where($priceExpr . ' >= ?', $from);
+            }
+            if ('' !== $to) {
+                if (Mage::getStoreConfigFlag('catalog/layered_navigation/price_subtraction')) {
+                    $select->where($priceExpr . ' < ?', $to);
+                } else {
+                    $select->where($priceExpr . ' <= ?', $to);
+                }
+            }
+        } else {
+            $range = $from;
+            $index = $to;
+
+            $collection = $filter->getLayer()->getProductCollection();
+            $collection->addPriceData($filter->getCustomerGroupId(), $filter->getWebsiteId());
+
+            $select     = $collection->getSelect();
+            $response   = $this->_dispatchPreparePriceEvent($filter, $select);
+
+            $table      = $this->_getIndexTableAlias();
+            $additional = join('', $response->getAdditionalCalculations());
+            $rate       = $filter->getCurrencyRate();
+            $priceExpr  = new Zend_Db_Expr("(({$table}.min_price {$additional}) * {$rate})");
+
+            $select->where($priceExpr . ' >= ?', ($range * ($index - 1)));
+
+            if (Mage::getStoreConfigFlag('catalog/layered_navigation/price_subtraction')) {
+                $select->where($priceExpr . ' < ?', ($range * $index));
+            } else {
+                $select->where($priceExpr . ' <= ?', ($range * $index));
             }
         }
 
-        if ('' !== $from) {
-            $select->where($priceExpr . ' >= ?', $from);
+        return $this;
+    }
+
+    public function applyPriceRange($filter)
+    {
+        $interval = $filter->getInterval();
+        if (!$interval) {
+            return $this;
         }
-        if ('' !== $to) {
-            $select->where($priceExpr . ' < ?', $to);
+
+        list($from, $to) = $interval;
+        if ($from === '' && $to === '') {
+            return $this;
+        }
+
+        $select = $filter->getLayer()->getProductCollection()->getSelect();
+        $priceExpr = $this->_getPriceExpression($filter, $select, false);
+
+        if ($to !== '') {
+            $to = (float)$to;
+            if ($from == $to) {
+                $to += self::MIN_POSSIBLE_PRICE;
+            }
+        }
+
+        if ($from !== '') {
+            $select->where($priceExpr . ' >= ' . $this->_getComparingValue($from, $filter));
+        }
+        if ($to !== '') {
+            if (Mage::getStoreConfigFlag('catalog/layered_navigation/price_subtraction')) {
+                $select->where($priceExpr . ' < ' . $this->_getComparingValue($to, $filter));
+            } else {
+                $select->where($priceExpr . ' <= ' . $to);
+            }
         }
 
         return $this;
+
     }
 }
